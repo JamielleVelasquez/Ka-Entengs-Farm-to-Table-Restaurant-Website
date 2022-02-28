0557a7737948334b4069d082a02f8a97da7c5054
Matt <83875278+Axiion@users.noreply.github.com>
2022-02-28 19:04:43 +0800

    Edited admin_login and tour_info pages. Set landing_page as default page. Added packages for controllers, listeners, and model. Added add reservation to database, and admin login function Very barebones so paki design nalang if kaya na hehe

---
+++ Ka-Entengs-Farm-to-Table-Restaurant-Website/src/java/controllers/reservationServlet.java
@@ -0,0 +1,138 @@
+package controllers;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Calendar;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class reservationServlet extends HttpServlet {
+
+    Connection con;
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        try {
+            //Register driver
+            Class.forName(config.getInitParameter("jdbcClassName"));
+            System.out.println("Loaded driver.");
+
+            //Use String buffer for connection
+            StringBuffer buff = new StringBuffer(config.getInitParameter("jdbcDriverURL"))
+                    .append("://").append(config.getInitParameter("dbHostName"))
+                    .append(":").append(config.getInitParameter("dbPort"))
+                    .append("/").append(config.getInitParameter("databaseName"));
+            //jdbc:derby://localhost:1527/KaEntengRestaurantToTableDB
+
+            //Establish connection
+            con = DriverManager.getConnection(buff.toString(),
+                    config.getInitParameter("dbUserName"), config.getInitParameter("dbPassword"));
+            System.out.println("You are now connected.");
+
+        } catch (SQLException sql) {
+            System.out.println("SQL Exception occurred.");
+            sql.printStackTrace();
+        } catch (ClassNotFoundException cnfe) {
+            System.out.println("Class not found Exception occurred.");
+            cnfe.printStackTrace();
+        }
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            ServletContext sc = getServletContext();
+            try {
+                if (con != null) {
+                    String fn = request.getParameter("resFn").trim();
+                    String ln = request.getParameter("resLn").trim();
+                    String cpNum = request.getParameter("resNum").trim();
+                    String email = request.getParameter("resEmail").trim();
+
+                    //checks if fields are empty
+                    if (fn.isEmpty()) {
+                        sc.setAttribute("errorMessage", "First name field is empty!");
+                        throw new SQLException(); //change this exception to user-defined exception soon, placeholder
+                    } else if (ln.isEmpty()) {
+                        sc.setAttribute("errorMessage", "Last name field is empty!");
+                        throw new SQLException(); //change this exception to user-defined exception soon, placeholder
+                    } else if (cpNum.isEmpty()) {
+                        sc.setAttribute("errorMessage", "Cellphone number field is empty!");
+                        throw new SQLException(); //change this exception to user-defined exception soon, placeholder
+                    } else if (email.isEmpty()) {
+                        sc.setAttribute("errorMessage", "Email field is empty!");
+                        throw new SQLException(); //change this exception to user-defined exception soon, placeholder
+                    }
+
+                    PreparedStatement pStmt = con.prepareStatement("INSERT INTO RESERVATIONDB (FNAME, LNAME, CPNUMBER, EMAIL) "
+                            + "VALUES (?, ?, ?, ?)");
+                    pStmt.setString(1, fn);
+                    pStmt.setString(2, ln);
+                    pStmt.setString(3, cpNum);
+                    pStmt.setString(4, email);
+
+                    pStmt.executeUpdate();
+                    response.sendRedirect("tour_info.html");
+                    pStmt.close();
+                    return;
+                } else {
+                    response.sendError(HttpServletResponse.SC_NOT_FOUND);
+                }
+            } catch (SQLException sqle) {
+                sqle.printStackTrace();
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Ka-Entengs-Farm-to-Table-Restaurant-Website/src/java/model/Admin.java
@@ -0,0 +1,24 @@
+package model;
+
+
+public class Admin {
+    private static String userEmail, email, password;
+
+    public Admin(String uNameEmail, String pWord) {
+        userEmail = uNameEmail;
+        password = pWord;
+    }
+
+    public void setDetails(String uNameEmail, String pWord) {
+        userEmail = uNameEmail;
+        password = pWord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
