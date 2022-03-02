1d21f9cfaad3b18f2f6e48b1eff74c4b6cb3e553
JamielleVelasquez <65398417+JamielleVelasquez@users.noreply.github.com>
2022-03-02 16:51:56 +0800

    Landing Page Front-end Finished
    Added Boostrap Icons
    Edited Headers and Footers
    Added Background Color Classes
    Added Styling for text tags
    Added Landing Page Styling
    Added Landing Page Copy
    Landing Page is now Responsive

--- script.js
+++ script.js
@@ -7,21 +7,30 @@ class Header extends HTMLElement {
 
     connectedCallback() {
         this.innerHTML = `
-        <header id="header">
-        <nav class="navbar justify-content-md-between">
-            <a class="navbar-brand h4 text-dark px-0 px-sm-4" href="landing_page.html">Enteng's Farm to Table Restaurant</a>
-            <ul class="list-group list-group-horizontal list-group-flush align-items-center">
-                <li class="list-group-item"><a href="about.html">About Us</a>
-                </li>
-                <li class="list-group-item"><a href="menu.html">Menu</a>
-                </li>
-                <li class="list-group-item"><a href="gallery.html">Gallery</a>
-                </li>
-                <li class="list-group-item"><a href="tour_info.html" class="btn btn-primary">Reserve</a>
-                </li>
-            </ul>
+        <header class="sticky-top bg-white" id="header">
+        <nav class="navbar navbar-expand-md p-0">
+            <div class="container-fluid">
+                <a class="navbar-brand h4 text-dark px-0 px-sm-4 d-none d-sm-block" href="landing_page.html">Enteng's Farm to Table Restaurant</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar-toggle">
+                    <i class="bi bi-list"></i>
+                </button>
+                <div class="collapse navbar-collapse flex-grow-0" id="navbar-toggle">
+                    <ul class="navbar-nav p-2">
+                        <li class="nav-item px-2 my-1"><a class="nav-link" href="about.html">About Us</a>
+                        </li>
+                        <li class="nav-item px-2 my-1"><a class="nav-link" href="menu.html">Menu</a>
+                        </li>
+                        <li class="nav-item px-2 my-1"><a class="nav-link" href="gallery.html">Gallery</a>
+                        </li>
+                        <li class="nav-item px-2 my-1"><a href="tour_info.html" class="btn btn-primary nav-link">Reserve a Tour</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+
         </nav>
-    </header>
+        </header>
     `;
     }
 }
@@ -35,21 +44,21 @@ class Footer extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
         <footer>
-        <div class="container-fluid p-5 text-center text-sm-start" id="footer">
+        <div class="container-fluid p-5 text-center text-md-start" id="footer">
             <div class="row">
-                <div class="col-12 col-sm-6">
+                <div class="col-12 g-3 col-md-6">
                     <h3> Enteng's Farm to Table Restaurant</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In efficitur aliquam arcu, vitae lobortis sapien pulvinar at.</p>
+                    <p>Enteng's Farm practices sustainable and organic farming. We take pride in using natural resources and natural methods to produce quality output.</p>
                 </div>
-                <div class="col-12 col-sm-2">
+                <div class="col-12 g-3 col-md-2">
                     <h3>Our Socials</h3>
                     <p>Placeholder</p>
                 </div>
-                <div class="col-12 col-sm-2">
+                <div class="col-12 g-3 col-md-2">
                     <h3>About Us</h3>
                     <p>Placeholder</p>
                 </div>
-                <div class="col-12 col-sm-2">
+                <div class="col-12 g-3 col-md-2">
                     <h3>Contact Us</h3>
                     <p>Placeholder</p>
                 </div>
